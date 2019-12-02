8df6ad908e84c31a58b44a15b0942cc123ebce54
wiśnia <pawel.wis.me@gmail.com>
2019-12-02 16:07:00 +0100

    DB connection config

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ RUN npm install
 COPY . .
 
 EXPOSE 3000
-CMD [ "node", "app.js" ]
+CMD [ "npm", "run", "prod" ]
--- config/db_config.js
+++ config/db_config.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose')
 
+console.log('Connection url => ', process.env.MONGO_URL);
 var host = process.env.MONGO_URL || 'localhost'
 var port = 27017
-var db_name = ''
+var db_name = 'firstapp_db'
 var options = {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }
 
-mongoose.connect(`mongodb://${host}:${port}/${db_name}`, options).then(
+console.log(`mongodb://root:root@${host}:${port}/${db_name}`)
+
+mongoose.connect(`mongodb://root:root@${host}:${port}/${db_name}`, options).then(
     () => { console.log('Database connection success.') }
 ).catch(
     (err) => { console.log(err) }
