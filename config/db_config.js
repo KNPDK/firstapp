924fb1234df58fc36f7e185ba0e63cacdd29dc29
wiśnia <pawel.wis.me@gmail.com>
2019-12-02 13:16:07 +0100

    Add connection options

--- config/db_config.js
+++ config/db_config.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose')
 var host = process.env.MONGO_URL || 'localhost'
 var port = 27017
 var db_name = ''
-var url
 var options = {
     useNewUrlParser: true
 }
 
-mongoose.connect(`mongodb://${host}:${port}/${db_name}`).then(
+mongoose.connect(`mongodb://${host}:${port}/${db_name}`, options).then(
     () => { console.log('Database connection success.') }
 ).catch(
     (err) => { console.log(err) }
