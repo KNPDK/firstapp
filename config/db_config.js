0ee9b04bb0cc1d5827fb190d962ece6168d85516
wiśnia <pawel.wis.me@gmail.com>
2019-12-02 16:18:45 +0100

    Add options to connection

--- config/db_config.js
+++ config/db_config.js
@@ -4,13 +4,9 @@ console.log('Connection url => ', process.env.MONGO_URL);
 var host = process.env.MONGO_URL || 'localhost'
 var port = 27017
 var db_name = 'firstapp_db'
-var options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
 
 
-mongodbClient.connect(`mongodb://root:root@${host}:${port}/${db_name}`).then(
+mongodbClient.connect(`mongodb://root:root@${host}:${port}/${db_name}?useUnifiedTopology=true`).then(
     () => { console.log('Database connection success.')}
 ).catch(
     (err) => {console.log(err)}
