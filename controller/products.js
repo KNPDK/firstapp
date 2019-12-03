9d5cbe498845c408a12577341802875543405ea3
Thomas-RC <tomekrosik@gmail.com>
2019-12-03 10:22:20 +0100

    First controller and db model

---
+++ controller/products.js
@@ -0,0 +1,15 @@
+const Products = require('../model/products');
+
+exports.getProducts = (req, res, next) => {
+    //todo:
+    // const product = new Products(req.body.prname, req.body.prprice, req.body.prdesc);
+    // product.findAll();
+    console.log('get products');
+};
+
+exports.postProducts = (req, res, next) => {
+    //todo:
+    // const product = new Products(req.body.prname, req.body.prprice, req.body.prdesc);
+    // product.add();
+    console.log('post products')
+};
